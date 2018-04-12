dd2a6f98216a0f06399023865134fd20814cfdfa
D7703_25 <d7703_25@D7703-25ui-iMac.local>
2018-04-12 11:12:20 +0900

    Stop Watch

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    var count = 0
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateTime() {
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+        
     }
-
-
+    @IBAction func btStart(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()
+            
+        })
+    }
+    
+    @IBAction func btStop(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    
+    @IBAction func btReset(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+        timeLabel.text = "00:00:00"
+    }
+    
+    
 }
-
